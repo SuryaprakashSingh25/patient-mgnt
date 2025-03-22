caa54a1cad04548889f61e9da78462fad0ec0e40
SuryaprakashSingh25 <singh.suraj1025@gmail.com>
2025-03-22 14:54:59 +0530

    Dockerise Patient Service and DB, Setup Billing service and GRPC

--- patient-service/src/main/java/com/pm/patient_service/model/Patient.java
+++ patient-service/src/main/java/com/pm/patient_service/model/Patient.java
@@ -3,13 +3,11 @@ package com.pm.patient_service.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
-@Data
 public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -22,6 +20,54 @@ public class Patient {
     @Email
     @Column(unique = true)
     private String email;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public @NotNull String getName() {
+        return name;
+    }
+
+    public void setName(@NotNull String name) {
+        this.name = name;
+    }
+
+    public @NotNull @Email String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NotNull @Email String email) {
+        this.email = email;
+    }
+
+    public @NotNull String getAddress() {
+        return address;
+    }
+
+    public void setAddress(@NotNull String address) {
+        this.address = address;
+    }
+
+    public @NotNull LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(@NotNull LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public @NotNull LocalDate getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(@NotNull LocalDate registeredDate) {
+        this.registeredDate = registeredDate;
+    }
 
     @NotNull
     private String address;
